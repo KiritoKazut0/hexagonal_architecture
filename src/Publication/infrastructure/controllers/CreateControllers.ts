0491844b42a7d0abe5f7baa21c0f9a3bcca605f5
KiritoKazut0 <norbertolopez291@gmail.com>
2024-05-24 12:47:39 -0600

    added create publication

--- src/Publication/infrastructure/controllers/CreateControllers.ts
+++ src/Publication/infrastructure/controllers/CreateControllers.ts
@@ -1,1 +1,20 @@
 import { Request, Response } from "express";
+import CreatePublicationCase from "../../aplication/CreatePublicationCase";
+import CreatePublicationRequest from "../../domain/DTOS/PublicationRequest";
+
+export default class CreateControllerPublication {
+    constructor (readonly createPublication: CreatePublicationCase) {}
+
+    async run (req: Request, res: Response){
+        const {contenido, id_usuario} = req.body;
+
+        const publication: CreatePublicationRequest = {
+            id_usuario,
+            contenido
+        };
+
+        const result = await this.createPublication.run(publication)
+
+    }
+
+}
